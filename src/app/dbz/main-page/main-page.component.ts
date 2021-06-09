efda99ff7ab9c22f63fcfa31cf014ea0fb73f3ef
Alfredo Neri <alfredo.neri@itbmexico.com>
2021-06-09 14:19:32 -0500

    seccion 5

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  nuevo:Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1110
+  }
+
+  get personajes():Personaje[]{
+    return this.dbzService.personajes;
+  }
+
+  agregarNuevoPersonaje(args: Personaje){
+    this.personajes.push(args);
+  }
+
+  constructor(private dbzService: DbzService){  }
+  
+}
